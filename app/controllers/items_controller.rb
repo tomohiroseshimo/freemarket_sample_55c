5d9fd146e1d52b9db5e5977c2eae423eb2dcd52c
tomohiroseshimo <oresenyouzaku2@gmail.com>
2019-10-14 16:26:53 +0900

    action definition
    sells controller 上でのnewアクション、createアクション、ストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,22 +2,22 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def create
-    # formのデータを受け取る
-    @item = Item.new(item_params)
-  end
+  # def create
+  #   # formのデータを受け取る
+  #   @item = Item.new(item_params)
+  # end
 
-  private
+  # private
 
-  def item_params
-    # ストロングパラメータ
-    params.require(:item).
-      permit(:name, :image, :category, :brand, :size, :condition, :cost,
-      :shipping, :area, :date, :price, :buyer_id, :saler_id)
-  end
+  # def item_params
+  #   # ストロングパラメータ
+  #   params.require(:item).
+  #     permit(:name, :image, :category, :brand, :size, :condition, :cost,
+  #     :shipping, :area, :date, :price, :buyer_id, :saler_id)
+  # end
 
-  def set_item
-    #データの取得
-    @item = Item.find(params[:id])
-  end
+  # def set_item
+  #   #データの取得
+  #   @item = Item.find(params[:id])
+  # end
 end
