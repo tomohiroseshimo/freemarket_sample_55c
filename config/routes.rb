9127340ff2846b37a19f785f5de2e8efe42a51f4
kiyoshioba <kiyoshioba.jpn@gmail.com>
2019-10-20 00:20:21 +0900

    routeの記述の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'items#index'
   get 'items' => 'items#index'
-  get "/items/sell" => "items#new"
+  get "/items/new" => "items#new"
   get 'mypages' => 'mypages#index'
   post 'items' => 'items#create'
 
