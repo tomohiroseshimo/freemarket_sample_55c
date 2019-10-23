1cfe9e84c398a17fb3c484b52080b0341688800a
tomohiroseshimo <oresenyouzaku2@gmail.com>
2019-10-23 22:57:34 +0900

    DBの作り直し
    buyer_idとsaler_idの追加

--- db/migrate/20190925043146_create_items.rb
+++ db/migrate/20190925043146_create_items.rb
@@ -1,18 +1,20 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-    t.string  :name,      null: false
-    t.string  :description, null:false
-    t.string  :image,     null: false
-    t.string  :category,  null: false
+    t.string  :name
+    t.string  :description
+    t.string  :image
+    t.string  :category 
     t.string  :brand
-    t.string  :size,      null: false
-    t.string  :condition, null: false
-    t.string  :cost,      null: false
-    t.string  :shipping,  null: false
-    t.string  :area,      null: false
-    t.string  :date,      null: false
-    t.integer :price,    null: false
+    t.string  :size
+    t.string  :condition
+    t.string  :cost
+    t.string  :shipping
+    t.string  :area
+    t.string  :date
+    t.integer :price
+    t.integer :buyer_id
+    t.integer :saler_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,18 +13,20 @@
 ActiveRecord::Schema.define(version: 2019_10_05_113641) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "description", null: false
-    t.string "image", null: false
-    t.string "category", null: false
+    t.string "name"
+    t.string "description"
+    t.string "image"
+    t.string "category"
     t.string "brand"
-    t.string "size", null: false
-    t.string "condition", null: false
-    t.string "cost", null: false
-    t.string "shipping", null: false
-    t.string "area", null: false
-    t.string "date", null: false
-    t.integer "price", null: false
+    t.string "size"
+    t.string "condition"
+    t.string "cost"
+    t.string "shipping"
+    t.string "area"
+    t.string "date"
+    t.integer "price"
+    t.integer "buyer_id"
+    t.integer "saler_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
