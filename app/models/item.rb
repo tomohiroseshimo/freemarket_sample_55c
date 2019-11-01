6ff3fe4d4bbc27ce996e784a7e9ef83f908d40ec
kiyoshioba <kiyoshioba.jpn@gmail.com>
2019-11-01 17:17:31 +0900

    アソシエーションを一旦コメントアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  belongs_to :buyer, class_name: "User"
-  belongs_to :saler, class_name: "User"
+  # belongs_to :user
+  # belongs_to :buyer, class_name: "User"
+  # belongs_to :saler, class_name: "User"
   has_many :items_categories
   has_many :images
   has_many :categories, through: :items_categories
