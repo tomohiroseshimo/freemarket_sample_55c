0328a60eb3ed18b7de42951ed7c0d113d0e236fa
tomohiroseshimo <oresenyouzaku2@gmail.com>
2019-11-03 15:07:26 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_113641) do
+
+ActiveRecord::Schema.define(version: 2019_11_02_141229) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
+
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "image", null: false
     t.string "category", null: false
     t.string "brand"
-    t.string "size", null: false
-    t.string "condition", null: false
-    t.string "cost", null: false
-    t.string "shipping", null: false
-    t.string "area", null: false
-    t.string "date", null: false
-    t.integer "price", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+
+    t.string "size"
+    t.string "condition"
+    t.string "cost"
+    t.string "shipping"
+    t.string "area"
+    t.string "date"
+    t.integer "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
