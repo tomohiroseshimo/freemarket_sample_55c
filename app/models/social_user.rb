8b9722c32a52e2becf2a7f106e9d6c8cb94d0e36
kagawa <tekken77marduk>
2019-11-06 08:34:30 +0900

    インデント修正

--- app/models/social_user.rb
+++ app/models/social_user.rb
@@ -4,26 +4,24 @@ class SocialUser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
-         def self.find_for_oauth(auth)
-          user = SocialUser.where(uid: auth.uid, provider: auth.provider).first
+  def self.find_for_oauth(auth)
+   user = SocialUser.where(uid: auth.uid, provider: auth.provider).first
       
-          unless user
-            user = SocialUser.create(
-              uid:      auth.uid,
-              provider: auth.provider,
-              email:    SocialUser.dummy_email(auth),
-              password: Devise.friendly_token[0, 20]
+    unless user
+     user = SocialUser.create(
+     uid:      auth.uid,
+     provider: auth.provider,
+     email:    SocialUser.dummy_email(auth),
+     password: Devise.friendly_token[0, 20]
             )
-          end
+      end
       
-          user
-        end
+        user
+      end
       
         private
       
-        def self.dummy_email(auth)
-          "#{auth.uid}-#{auth.provider}@example.com"
-        end
-      
-              
-end
+    def self.dummy_email(auth)
+      "#{auth.uid}-#{auth.provider}@example.com"
+    end
+  end
