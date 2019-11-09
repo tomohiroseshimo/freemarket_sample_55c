405513fcfb9d5b4421cca4d9a980c988ea76c384
ashima8 <ve_sha_t@yahoo.co.jp>
2019-11-09 21:31:28 +0900

    deploy.rbに追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,3 +52,5 @@ end
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
