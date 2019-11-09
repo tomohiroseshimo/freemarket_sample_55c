eab07c992dd94d98250a02508a30d388dc4a0e18
yuta-ksksr <yuta.tkhs.h2o@gmail.com>
2019-11-09 21:30:13 +0900

    first-commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def detail
+    render "items/_details"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
-# Rails.application.routes.draw do
-#   get 'items' => 'items#index'
-#   root 'mypages#index'
-# end
-
 Rails.application.routes.draw do
   root 'items#index'
   get 'items' => 'items#index'
   get 'mypage' => 'mypages#index'
   get 'mypage/identification' =>'mypages#identification'
+  get 'items/detail' => 'items#detail'
 end
