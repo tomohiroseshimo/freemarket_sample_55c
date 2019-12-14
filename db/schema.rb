874a6caf18cb0e5909ffde1eb45811587c44520d
ashima8 <ve_sha_t@yahoo.co.jp>
2019-12-14 20:49:22 +0900

    データベースを作成する前にusersテーブルを削除するように

--- db/schema.rb
+++ db/schema.rb
@@ -49,41 +49,4 @@ ActiveRecord::Schema.define(version: 2019_11_18_111024) do
     t.index ["reset_password_token"], name: "index_social_users_on_reset_password_token", unique: true
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "email", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.integer "birthdate_year", null: false
-    t.integer "birthdate_month", null: false
-    t.integer "birthdate_day", null: false
-    t.integer "phone_number", null: false
-    t.string "address_last_name", null: false
-    t.string "address_first_name", null: false
-    t.string "address_last_name_kana", null: false
-    t.string "address_first_name_kana", null: false
-    t.string "address_number", null: false
-    t.integer "address_prefecture", default: 0, null: false
-    t.string "address_name", null: false
-    t.string "address_block", null: false
-    t.string "address_building"
-    t.integer "address_phone_number"
-    t.text "introduce"
-    t.string "encrypted_password", default: "", null: false
-    t.string "card_number", null: false
-    t.string "security_number", null: false
-    t.string "effectiveness_month", null: false
-    t.string "effectiveness_year", null: false
-    t.string "authentication_number", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
