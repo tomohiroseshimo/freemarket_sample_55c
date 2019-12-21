dda79173490995b3641aecf42222866ef77c3f4c
yuta-ksksr <yuta.tkhs.h2o@gmail.com>
2019-12-21 12:25:06 +0900

    JavaScript_comment-out

--- app/assets/javascripts/itemImage.js
+++ app/assets/javascripts/itemImage.js
@@ -1,32 +1,16 @@
-  $(function(){
-    $('.photos--thumb__piece').mouseover(function(){
-        //オーバーしたliのインデックスを取得
-        var index = $('.photos--thumb__piece').index(this);
-        //オーバーした画像URLを取得
-        var imageurl = $('.photos--thumb__piece').eq(index).find('img').attr('src');
-        //ulのクラス名を取得（空白で分割）
-        const className = $(this).attr('class').split(" ");
-        //元のメイン画像を保存しておく
-        var defaultImage = $('.img.photos--main__piece.'+className).attr('src');
-        $('.img.photos--main__piece.'+className).attr('src',imageurl);
-    },function(){
-        $('.img.photos--main__piece.'+className).attr('src',defaultImage);
-    });
-  });
-
-
-// $(function(){
-//   $('.subimage li').hover(function(){
-//       //オーバーしたliのインデックスを取得
-//       var index = $('.subimage li').index(this);
-//       //オーバーした画像URLを取得
-//       var imageurl = $('.subimage li').eq(index).find('img').attr('src');
-//       //ulのクラス名を取得（空白で分割）
-//       className = $(this).parent().attr('class').split(" ");
-//       //元のメイン画像を保存しておく
-//       defaultImage = $('img.mainimage.'+className[1]).attr('src');
-//       $('img.mainimage.'+className[1]).attr('src',imageurl);
-//   },function(){
-//       $('img.mainimage.'+className[1]).attr('src',defaultImage);
-//   });
-// })
+// 商品情報詳細ページの画像。変数defaultImageがundefined。マウスオーバーでサムネイルとメインを入れ替えたかった
+  // $(function(){
+  //   $('.photos--thumb__piece').mouseover(function(){
+  //       //オーバーしたliのインデックスを取得
+  //       var index = $('.photos--thumb__piece').index(this);
+  //       //オーバーした画像URLを取得
+  //       var imageurl = $('.photos--thumb__piece').eq(index).find('img').attr('src');
+  //       //ulのクラス名を取得（空白で分割）
+  //       const className = $(this).attr('class').split(" ");
+  //       //元のメイン画像を保存しておく
+  //       var defaultImage = $('.img.photos--main__piece.'+className).attr('src');
+  //       $('.img.photos--main__piece.'+className).attr('src',imageurl);
+  //   },function(){
+  //       $('.img.photos--main__piece.'+className).attr('src',defaultImage);
+  //   });
+  // });
