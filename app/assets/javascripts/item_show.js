5e1df3e298bf946c94cee64c3045460138a93295
yuta-ksksr <yuta.tkhs.h2o@gmail.com>
2020-02-06 20:50:23 +0900

    delete-turbolinks

--- app/assets/javascripts/item_show.js
+++ app/assets/javascripts/item_show.js
@@ -1,18 +1,16 @@
-$(document).on('turbolinks:load', function() {
-  $(function() {
+$(function() {
 
-    $('.photos--thumb__piece').mouseover(function(){
+  $('.photos--thumb__piece').mouseover(function(){
 
-      var leftSide = $('.photos--thumb').children('li').index(this);
-      var rightSide = $('.photos--main').children('li').index(this);
+    var leftSide = $('.photos--thumb').children('li').index(this);
+    var rightSide = $('.photos--main').children('li').index(this);
 
-      if (leftSide != rightSide) {
-        if (rightSide <= leftSide) {
-          $('.photos--main').stop().animate({left: -(leftSide)*300}, 300)
-        } else {
-          $('.photos--main').stop().animate({left: leftSide*300}, 300)
-        }
-      };
-    });
+    if (leftSide != rightSide) {
+      if (rightSide <= leftSide) {
+        $('.photos--main').stop().animate({left: -(leftSide)*300}, 300)
+      } else {
+        $('.photos--main').stop().animate({left: leftSide*300}, 300)
+      }
+    };
   });
 });
