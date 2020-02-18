48625244a4ad51fdcd380348ad30e114603c9f0d
yuta-ksksr <yuta.tkhs.h2o@gmail.com>
2020-02-18 20:20:43 +0900

    新規会員登録画面のリンク追加
    アイコンをlinkに変更

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,6 @@ get 'commodity/:id' => 'mypages#commodity'
 get 'items' => 'items#index'
 get "items/new" => "items#new"
 post 'items' => 'items#create'
-# get 'show/:id' => 'items#show'
 get "items/:id/edit" => "items#edit"
 get "after/:id" => "items#after",as: 'after'
 delete 'items/:id' => 'items#destroy'
