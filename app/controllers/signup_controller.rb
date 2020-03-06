93bc9b1c852569012eab510bdb48a5c2a81d70be
kagawa <tekken77marduk>
2020-03-06 17:58:58 +0900

    バリデーション

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,9 @@
 class SignupController < ApplicationController
+  before_action :validates_telephone_number, only: :telephone_number 
+  before_action :validates_telephone_auther, only: :telephone_auther 
+  before_action :validates_address, only: :address
+  before_action :validates_pay, only: :pay 
+  
   def index
   end  
 
@@ -9,6 +14,28 @@ class SignupController < ApplicationController
  
   
   def telephone_number
+   @user = User.new # 新規インスタンス作成
+  end
+  
+  def telephone_auther
+    @user = User.new # 新規インスタンス作成
+  end
+
+  def address
+    @user = User.new 
+  end
+  
+  def pay
+    @user = User.new 
+  end
+
+  # def user_data
+  #   @user = User.new # 新規インスタンス作成
+  # end
+
+ 
+  
+  def validates_telephone_number
     # step1で入力された値をsessionに保存
    session[:nickname] = user_params[:nickname]
    session[:email] = user_params[:email]
@@ -21,22 +48,112 @@ class SignupController < ApplicationController
    session[:birthdate_year] = user_params[:birthdate_year]
    session[:birthdate_month] = user_params[:birthdate_month]
    session[:birthdate_day] = user_params[:birthdate_day]
-   session[:first_name_kana] = user_params[:first_name_kana]
-   @user = User.new # 新規インスタンス作成
+   @user = User.new(
+    nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
+    email: session[:email],
+    password: session[:password],
+    password_confirmation: session[:password_confirmation],
+    last_name: session[:last_name], # 入力前の情報は、バリデーションに通る値を仮で入れる
+    first_name: session[:first_name], 
+    last_name_kana: session[:last_name_kana], 
+    first_name_kana: session[:first_name_kana], 
+    birthdate_year: session[:birthdate_year],
+    birthdate_month: session[:birthdate_month],
+    birthdate_day: session[:birthdate_day],
+    phone_number: "111",
+    authentication_number: "111",
+    address_number: "111",
+    address_prefecture: "1",
+    address_first_name: "千葉",
+    address_last_name: "千葉",
+    address_first_name_kana: "チバ",
+    address_last_name_kana: "チバ",
+    address_block: "千葉",
+    address_building: "千葉",
+    address_phone_number: "111",
+    address_name: "千葉",
+    encrypted_password: "zxcvbn",
+    card_number: "111",
+    security_number: "111",
+    effectiveness_month: "1",
+    effectiveness_year: "2020"
+  )
+   render '/signup/user_data' unless @user.valid?
+   @user.errors
   end
   
-  def telephone_auther
+  def validates_telephone_auther
     # step2で入力された値をsessionに保存
     session[:phone_number] = user_params[:phone_number]
-    @user = User.new # 新規インスタンス作成
+    @user = User.new(
+      nickname: session[:nickname],
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      last_name: session[:last_name], 
+      first_name: session[:first_name], 
+      last_name_kana: session[:last_name_kana], 
+      first_name_kana: session[:first_name_kana], 
+      birthdate_year: session[:birthdate_year],
+      birthdate_month: session[:birthdate_month],
+      birthdate_day: session[:birthdate_day],
+      phone_number: session[:phone_number],
+      authentication_number: "111",
+      address_number: "111",
+      address_prefecture: "1",
+      address_first_name: "千葉",
+      address_last_name: "千葉",
+      address_first_name_kana: "チバ",
+      address_last_name_kana: "チバ",
+      address_block: "千葉",
+      address_building: "千葉",
+      address_phone_number: "111",
+      address_name: "千葉",
+      encrypted_password: "zxcvbn",
+      card_number: "111",
+      security_number: "111",
+      effectiveness_month: "1",
+      effectiveness_year: "2020"
+    )
+    render '/signup/telephone_number' unless @user.valid?
   end
 
-  def address
+  def validates_address
     session[:authentication_number] = user_params[:authentication_number]
-    @user = User.new 
+    @user = User.new(
+      nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      last_name: session[:last_name], # 入力前の情報は、バリデーションに通る値を仮で入れる
+      first_name: session[:first_name], 
+      last_name_kana: session[:last_name_kana], 
+      first_name_kana: session[:first_name_kana], 
+      birthdate_year: session[:birthdate_year],
+      birthdate_month: session[:birthdate_month],
+      birthdate_day: session[:birthdate_day],
+      phone_number: session[:phone_number],
+      authentication_number: session[:authentication_number],
+      address_number: "111",
+      address_prefecture: "1",
+      address_first_name: "千葉",
+      address_last_name: "千葉",
+      address_first_name_kana: "チバ",
+      address_last_name_kana: "チバ",
+      address_block: "千葉",
+      address_building: "千葉",
+      address_phone_number: "111",
+      address_name: "千葉",
+      encrypted_password: "zxcvbn",
+      card_number: "111",
+      security_number: "111",
+      effectiveness_month: "1",
+      effectiveness_year: "2020"
+    ) 
+    render '/signup/telephone_auther' unless @user.valid?
   end
   
-  def pay
+  def validates_pay
     session[:address_number] = user_params[:address_number]
     session[:address_prefecture] = user_params[:address_prefecture]
     session[:address_first_name] = user_params[:address_first_name]
@@ -47,7 +164,37 @@ class SignupController < ApplicationController
     session[:address_building] = user_params[:address_building]
     session[:address_phone_number] = user_params[:address_phone_number]  
     session[:address_name] = user_params[:address_name]
-    @user = User.new 
+    @user = User.new(
+      nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      last_name: session[:last_name], # 入力前の情報は、バリデーションに通る値を仮で入れる
+      first_name: session[:first_name], 
+      last_name_kana: session[:last_name_kana], 
+      first_name_kana: session[:first_name_kana], 
+      birthdate_year: session[:birthdate_year],
+      birthdate_month: session[:birthdate_month],
+      birthdate_day: session[:birthdate_day],
+      phone_number: session[:phone_number],
+      authentication_number: session[:authentication_number],
+      address_number: session[:address_number],
+      address_prefecture: session[:address_prefecture],
+      address_first_name: session[:address_first_name],
+      address_last_name:  session[:address_last_name],
+      address_first_name_kana: session[:address_first_name_kana],
+      address_last_name_kana: session[:address_last_name_kana],
+      address_block: session[:address_block],
+      address_building: session[:address_building],
+      address_phone_number: session[:address_phone_number],
+      address_name: session[:address_name],
+      encrypted_password: "zxcvbn",
+      card_number: "111",
+      security_number: "111",
+      effectiveness_month: "1",
+      effectiveness_year: "2020"
+    ) 
+    render '/signup/address' unless @user.valid?
   end
 
  
@@ -88,8 +235,12 @@ class SignupController < ApplicationController
       session[:id] = @user.id
       redirect_to endpage_signup_index_path
     else
-      render 'devise/registrations/new'
+      render  '/signup/pay' 
     end
+  end
+
+  def endpage
+    sign_in User.find(session[:id]) unless user_signed_in?
   end
 
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,32 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthdate_year, presence: true
+  validates :birthdate_month, presence: true
+  validates :birthdate_day, presence: true
+  validates :phone_number, presence: true
+  validates :address_last_name, presence: true
+  validates :address_first_name, presence: true
+  validates :address_last_name_kana, presence: true
+  validates :address_first_name_kana, presence: true
+  validates :address_number, presence: true
+  validates :address_prefecture, presence: true
+  validates :address_name, presence: true
+  validates :address_block, presence: true
+  validates :encrypted_password, presence: true
+  validates :card_number, presence: true
+  validates :security_number, presence: true
+  validates :effectiveness_month, presence: true
+  validates :effectiveness_year, presence: true
+  validates :authentication_number, presence: true
+  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
